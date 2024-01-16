401a05b837a62de817914fea5d3539c5eb60b671
Junglepanda69 <Conrady82@gmail.com>
2024-01-16 11:55:00 -0800

    Initial Commit

---
+++ ReadWell/ReadWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadWellApp.swift
+//  ReadWell
+//
+//  Created by William Conrad Preston on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
